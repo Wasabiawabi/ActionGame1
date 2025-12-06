e116fb2b5be9a6d67f64030ae8441b4524f194fa
jinichiro-suzuki <Wasabi_Awabi@outlook.jp>
2025-12-06 12:48:59 +0900

    ステージオブジェクトの開発

--- Assets/Scripts/StageObject/StageObjectMovementHandler.cs
+++ Assets/Scripts/StageObject/StageObjectMovementHandler.cs
@@ -9,21 +9,36 @@ public class StageObjectMovementHandler : MonoBehaviour
 
     //スクリプトなど
     private Camera mainCamera;
+    private PlayerBuffHandler playerBuffHandler;
+    private DataSearchHandler dataSearchHandler;
     //変数
     private int objID;
     private float playerXSpeed;
     private float minWindY = 3f;
+    private float minY = 0f;
     private float minBallonPlaneY = 10f;
     private float minBallonX = 3f;
     private float minPlaneX = 10f;
     private bool isInitialized = false;
     private Vector3 moveSpeed;
+    private bool[] availableBuff = { false, false, false };
+    private int availableBuffCnt = 0;
+    private int buffid = 0;
+    float speedIncremnt;
+    float speedMultiply;
+    float maintainSpeedtime;
+    float multipleBuffPercent;
+    int maxMultipleBuff;
+    float increamentBuffMultiplyer;
+    float multipleBuffMultiplyer;
 
-    public void Initialize(int id, float playerSpeed, Camera mainCamera) // ステージオブジェクトの初期化
+    public void Initialize(int id, float playerSpeed, Camera mainCamera, PlayerBuffHandler playerBuffHandler, DataSearchHandler dataSearchHandler) // ステージオブジェクトの初期化
     {
         this.objID = id;
         this.playerXSpeed = playerSpeed;
         this.mainCamera = mainCamera ?? Camera.main; // フォールバック
+        this.playerBuffHandler = playerBuffHandler ?? null; // フォールバック
+        this.dataSearchHandler = dataSearchHandler ?? null; // フォールバック
         //Debug.Log("minWindY" + minWindY + "minBallonPlaneY" + minBallonPlaneY);
 
         if (this.mainCamera == null)
@@ -33,7 +48,8 @@ public class StageObjectMovementHandler : MonoBehaviour
 
         CalcYAxis();
         CalcMoveSpeed();
-
+        CalcBuff();
+        buffid = Random.Range(0, availableBuffCnt - 1); //  与えるバフの種類をここで決める
         isInitialized = true;
         // デバッグログ（呼び出し確認）
         //Debug.Log($"StageObjectMovementHandler.Initialize called: id={id}, playerSpeed={playerSpeed}, mainCamera={(this.mainCamera!=null?"ok":"null")}");
@@ -50,10 +66,25 @@ public class StageObjectMovementHandler : MonoBehaviour
         // 安全： mainCamera が null の場合は簡易固定 Y を使う
         var cam = mainCamera ?? Camera.main;
 
-        if (objID == 0 || objID == 1 || objID == 2) // 花、サトウキビ、蜂の巣の場合、地面に接地させる
+        if (objID == 0) // 花、蜂の巣の場合、地面に接地させる
         {
-            float randY = Random.Range(-2f, -1f);
+            float randY = Random.Range(-1.5f, -0.5f);
             transform.position = new Vector3(transform.position.x, randY, transform.position.z);
+            return;
+        }
+        else if (objID == 2) // 蜂の巣の場合、地面から少し浮かせる
+        {
+            float rand = Random.Range(0f, 3f);
+            transform.position = new Vector3(transform.position.x, rand, transform.position.z);
+            return;
+        }
+
+        if (objID == 1) // 粒の場合、自由に空中に浮かせる
+        {
+            float rand = Random.Range(0f, 1f);
+            float y = (cam != null) ? cam.ViewportToWorldPoint(new Vector3(0, rand, 0)).y : 0f;
+            if (y < minY) y = minY;
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
             return;
         }
 
@@ -85,7 +116,7 @@ public class StageObjectMovementHandler : MonoBehaviour
 
     private void CalcMoveSpeed() // IDごとに移動速度を計算する
     {
-        // 花、サトウキビ、蜂の巣の場合、速度は0
+        // 花、粒、蜂の巣の場合、速度は0
         if (objID == 0 || objID == 1 || objID == 2) moveSpeed = Vector3.zero;
         // 空中の場合、定速
         else if (objID == 3) moveSpeed = new Vector3(1, 0, 0);
@@ -93,5 +124,29 @@ public class StageObjectMovementHandler : MonoBehaviour
         else if (objID == 4) moveSpeed = new Vector3(Mathf.Max(playerXSpeed / 10f, minBallonX), 0, 0);
         // 飛行機の場合、移動速度の最低値か、プレイヤーの半分(暫定)
         else if (objID == 5) moveSpeed = new Vector3(Mathf.Max(playerXSpeed / 2f, minPlaneX), 0, 0);
+    }
+
+    private void CalcBuff() //  自身が消費されたときに与えられ得るバフを計算する
+    {
+        // 自身が与えるバフ効果の初期値を取得する
+        var data = dataSearchHandler.stageObjectStatusDataStore.GetDataByID(objID);
+        speedIncremnt     = data.SpeedIncrement;
+        speedMultiply     = data.SpeedMultiply;
+        maintainSpeedtime = data.SaveSpeedTime;
+
+        if (speedIncremnt > 0) availableBuff[0] = true;
+        if (speedMultiply > 0) availableBuff[1] = true;
+        if (maintainSpeedtime > 0) availableBuff[2] = true;
+
+        // 自身のバフの性能に関わるアップグレードのレベルを取得する
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //  プレイヤーが当たったときにバフを付与する
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
