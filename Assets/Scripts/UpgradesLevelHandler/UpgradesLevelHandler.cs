139854ab7d94aabced69eb156582f9e37d69dac3
jinichiro-suzuki <Wasabi_Awabi@outlook.jp>
2026-01-20 21:42:54 +0900

    所持金が足りない時はアップグレードできないようにした

--- Assets/Scripts/UpgradesLevelHandler/UpgradesLevelHandler.cs
+++ Assets/Scripts/UpgradesLevelHandler/UpgradesLevelHandler.cs
@@ -32,6 +32,9 @@ public class UpgradesLevelHandler : MonoBehaviour
     public StageObjectUpgradeData stageObjectUpgradeData;
     private JsonFileHandler jsonFileHandler = new JsonFileHandler();
 
+    [SerializeField] private MoneyManager moneyManager;
+    [SerializeField] private DataSearchHandler dataSearchHandler;
+
     //Pathを指定
     string path_playerData = Path.Combine(Application.dataPath, "Data/Json/playerData.json");
     string path_stageObjectData = Path.Combine(Application.dataPath, "Data/Json/stageObjectData.json");
@@ -66,36 +69,71 @@ public class UpgradesLevelHandler : MonoBehaviour
 
     public void UpgradePlayerMaxSpeed()
     {
+        if(moneyManager.totalMoney < dataSearchHandler.upgradeDataStore.GetDataByName("IncreaseMaxSpeed").UpgradeCostList[playerUpgradeData.increaseMaxSpeedLevel])
+        {
+            Debug.Log("Not enough money to upgrade Max Speed.");
+            return;
+        }
         playerUpgradeData.increaseMaxSpeedLevel += 1;
     }
 
     public void UpgradePlayerMaxInitialMovementSpeed()
     {
+        if (moneyManager.totalMoney < dataSearchHandler.upgradeDataStore.GetDataByName("IncreaseMaxInitialMovementSpeed").UpgradeCostList[playerUpgradeData.increaceMaxInitialMovementSpeedLevel])
+        {
+            Debug.Log("Not enough money to upgrade Max Initial Movement Speed.");
+            return;
+        }
         playerUpgradeData.increaceMaxInitialMovementSpeedLevel += 1;
     }
 
     public void UpgradePlayerSpeedDumping()
     {
+        if (moneyManager.totalMoney < dataSearchHandler.upgradeDataStore.GetDataByName("DecreaceSpeedDumping").UpgradeCostList[playerUpgradeData.decreaceSpeedDumpingLevel])
+        {
+            Debug.Log("Not enough money to upgrade Speed Dumping.");
+            return;
+        }
         playerUpgradeData.decreaceSpeedDumpingLevel += 1;
     }
 
     public void UpgradeStageObjectInstantiateProbabilityPerFrame()
     {
+        if (moneyManager.totalMoney < dataSearchHandler.upgradeDataStore.GetDataByName("IncreaseInstantiateProbabilityPerFrame").UpgradeCostList[stageObjectUpgradeData.increaseInstantiateProbabiltyPerFrameLevel])
+        {
+            Debug.Log("Not enough money to upgrade Instantiate Probability Per Frame.");
+            return;
+        }
         stageObjectUpgradeData.increaseInstantiateProbabiltyPerFrameLevel += 1;
     }
 
     public void UpgradeStageObjectMultipleBuffProbability()
     {
+        if (moneyManager.totalMoney < dataSearchHandler.upgradeDataStore.GetDataByName("IncreaseMultipleBuffProbability").UpgradeCostList[stageObjectUpgradeData.increaseMultipleBuffProbablityLevel])
+        {
+            Debug.Log("Not enough money to upgrade Multiple Buff Probability.");
+            return;
+        }
         stageObjectUpgradeData.increaseMultipleBuffProbablityLevel += 1;
     }
 
     public void UpgradeStageObjectMaxMultipleBuff()
     {
+        if (moneyManager.totalMoney < dataSearchHandler.upgradeDataStore.GetDataByName("MaxMultipleBuff").UpgradeCostList[stageObjectUpgradeData.maxMultipleBuffLevel])
+        {
+            Debug.Log("Not enough money to upgrade Max Multiple Buff.");
+            return;
+        }
         stageObjectUpgradeData.maxMultipleBuffLevel += 1;
     }
 
     public void UpgradeStageObjectBuffEffect()
     {
+        if (moneyManager.totalMoney < dataSearchHandler.upgradeDataStore.GetDataByName("IncreaseBuffEffect").UpgradeCostList[stageObjectUpgradeData.increaseBuffEffect])
+        {
+            Debug.Log("Not enough money to upgrade Buff Effect.");
+            return;
+        }
         stageObjectUpgradeData.increaseBuffEffect += 1;
     }
 }
