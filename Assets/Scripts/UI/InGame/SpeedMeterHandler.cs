368c84afb463bfb13c58fdc7cc29bdaddb8f80b8
jinichiro-suzuki <Wasabi_Awabi@outlook.jp>
2026-01-28 16:57:42 +0900

    スピードに応じてスライダーを上下させ、速度維持状態のときは特別な描画を行うようにした

--- Assets/Scripts/UI/InGame/SpeedMeterHandler.cs
+++ Assets/Scripts/UI/InGame/SpeedMeterHandler.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.UI;
+using System.Diagnostics;
 
 public class SpeedMeterHandler : MonoBehaviour
 {
@@ -12,12 +14,29 @@ public class SpeedMeterHandler : MonoBehaviour
     /// </summary>
     // スクリプト
     [SerializeField] private PlayerMovementHandler playerMovementHandler;
+    [SerializeField] private PlayerBuffHandler playerBuffHandler;
+    [SerializeField] private PlayerStatus playerStatus;
 
     // テキスト
     [SerializeField] private TextMeshProUGUI speedText;
 
+    // UIの画像
+    [SerializeField] private Sprite speedMeter;
+    [SerializeField] private Sprite cappedSpeedMeter;
+
+    // 画像を適用するコンポーネント
+    [SerializeField] private Image speedMeterImage;
+
+    // スライダー
+    [SerializeField] private Slider speedSlider;
+
     private void Update()
     {
         speedText.text = MathF.Floor(playerMovementHandler.playerSpeed).ToString() + " m/s";
+
+        if (playerBuffHandler.maintaining)speedMeterImage.sprite = cappedSpeedMeter;
+        else speedMeterImage.sprite = speedMeter;
+
+        speedSlider.value = playerMovementHandler.playerSpeed / playerStatus.playerStatusValue[playerStatus.playerStatusName.IndexOf("maxSpeed")];
     }
 }
