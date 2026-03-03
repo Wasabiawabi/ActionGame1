236a13ae4f16c2023a720373aeae0e47ff69f3ef
jinichiro-suzuki <Wasabi_Awabi@outlook.jp>
2026-03-03 12:13:35 +0900

    一括セーブ機能

--- Assets/Scripts/UI/SaveData.cs
+++ Assets/Scripts/UI/SaveData.cs
@@ -6,10 +6,27 @@ public class SaveData : MonoBehaviour
 {
 
     [SerializeField] private UpgradesLevelHandler upgradesLevelHandler;
+    [SerializeField] private MoneyManager moneyManager;
     
     public void SaveUpgradesData()
     {
         Debug.Log("saved");
         upgradesLevelHandler.SaveUpgradesDataFile();
+    }
+
+    public void SaveMoneyData()
+    {
+        PlayerPrefs.SetFloat("Money", moneyManager.totalMoney);
+    }
+
+    public void SaveAll()
+    {
+        SaveUpgradesData();
+        SaveMoneyData();
+    }
+
+    public void Start()
+    {
+        moneyManager.totalMoney = PlayerPrefs.GetFloat("Money", 0f);
     }
 }
