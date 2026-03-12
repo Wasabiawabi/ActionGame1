9a27445eff389eb4265e12ebf21bd80b99cc00dc
jinichiro-suzuki <Wasabi_Awabi@outlook.jp>
2026-03-12 22:50:43 +0900

    実行内容が散らかっていたため整理

--- Assets/Scripts/Player/MoneyManager.cs
+++ Assets/Scripts/Player/MoneyManager.cs
@@ -1,10 +1,35 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HandmadeLibrary.Json;
+
+
+[System.Serializable]
+public class TotalMoney
+{
+    [HideInInspector] public float totalMoney;
+}
 
 public class MoneyManager : MonoBehaviour
 {
     [Tooltip("金を管理する.")]
     bool summary;
+
+    private JsonFileHandler jsonFileHandler = new JsonFileHandler();
+    private TotalMoney totalMoneyData = new TotalMoney();
+
     public float totalMoney;
+    string path_totalMoney = Path.Combine(Application.dataPath, "Data/Json/totalMoney.json");
+
+    private void Start()
+    {
+        totalMoneyData = jsonFileHandler.ReadFromJson<TotalMoney>(path_totalMoney, new TotalMoney());
+        totalMoney = totalMoneyData.totalMoney;
+    }
+
+    public void SaveTotalMoney()
+    {
+        jsonFileHandler.SaveToJson<TotalMoney>(path_totalMoney, totalMoneyData);
+    }
 }
